3a0fec9d504d1345f36106b52bd9b0ad170840e4
Krystel <krystel@phonebooky.com>
2023-10-27 19:07:28 +0000

    Breaking the lambda function on purpose

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -12,15 +12,17 @@
  */
 
 export const lambdaHandler = async (event, context) => {
-    try {
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: "I'm using canary deployments",
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  throw new Error("This will cause a deployment rollback")
+
+    // try {
+    //     return {
+    //         'statusCode': 200,
+    //         'body': JSON.stringify({
+    //             message: "I'm using canary deployments",
+    //         })
+    //     }
+    // } catch (err) {
+    //     console.log(err);
+    //     return err;
+    // }
 };
